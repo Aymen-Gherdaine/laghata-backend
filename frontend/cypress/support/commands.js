a2ea5e40c0f671d9eec8f241fca4b676566cddca
AYMEN <gherdaineaymen1995@gmail.com>
2024-02-01 20:40:49 -0500

    Seaparate backend from frontend

--- frontend/cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-// Login to the app
-Cypress.Commands.add("loginToApplication", () => {
-  cy.visit("/login");
-  cy.get('[placeholder="Email"]').type(Cypress.env("userEmail"));
-  cy.get('[placeholder="Password"]').type(Cypress.env("userPassword"));
-  cy.get("form").submit();
-});
